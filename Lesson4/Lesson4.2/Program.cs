403f96ecaa34a82092c06f0614a123cd701bbf00
selotec <selotec@gmail.com>
2016-04-28 00:04:28 +0600

    Tasks from lesson 4

---
+++ Lesson4/Lesson4.2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4._2
+{
+    class Program
+    {
+        public class Phone
+        {
+            private readonly string _code;
+            private readonly string _number;
+
+            public string Code
+            {
+                get { return _code; }
+            }
+            public string Number
+            {
+                get { return _number; }
+            }
+            public Phone(string number, string code)
+            {
+                _code = code;
+                _number = number;
+            }
+            public void PrintNumber()
+            {
+                string outline = "";
+                if (_code != "")
+                {
+                    outline += string.Format("({0})", _code);
+                }
+                outline += string.Format("{0}", _number);
+                Console.WriteLine("Formated number:");
+                Console.WriteLine(outline);
+            }
+        }
+        static void Main(string[] args)
+        {
+            string code = "";
+            string number = "";
+            Console.WriteLine("Type code:");
+            code = Console.ReadLine();
+            Console.WriteLine("Type number:");
+            number = Console.ReadLine();
+            Phone o = new Phone(number, code);
+            o.PrintNumber();
+        }
+    }
+}
---
+++ Lesson4/Lesson4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4
+{
+    class Program
+    {
+        public class Phone
+        {
+            public readonly string city_code;
+            public readonly string phone_number;
+            public Phone(string number, string code)
+            {
+                city_code = code;
+                phone_number = number;
+            }
+            public void PrintNumber()
+            {
+                string outline = "";
+                if (city_code != "")
+                {
+                    outline += string.Format("({0})", city_code);
+                }
+                outline += string.Format("{0}", phone_number);
+                Console.WriteLine("Formated number:");
+                Console.WriteLine(outline);
+            }
+        }
+        static void Main(string[] args)
+        {
+            string code = "";
+            string number = "";
+            Console.WriteLine("Type code:");
+            code = Console.ReadLine();
+            Console.WriteLine("Type number:");
+            number = Console.ReadLine();
+            Phone o = new Phone(number, code);
+            o.PrintNumber();
+        }
+    }
+}
