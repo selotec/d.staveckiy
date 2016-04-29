b571c4f29f1046ed32dafb8c93b48fd3de6cd8ab
selotec <selotec@gmail.com>
2016-04-29 22:21:07 +0600

    Tasks from lesson 3

---
+++ Lesson3.1/Lesson3.1/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3._1
+{
+    class Program
+    {
+        public static int ReadIntFromConsole()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        public static double ReadDoubleFromConsole()
+        {
+            return Convert.ToDouble(Console.ReadLine());
+        }
+
+        public static void PrintInt(int number)
+        {
+            Console.WriteLine($"{number}");
+        }
+
+        public static void PrintDouble(double number)
+        {
+            Console.WriteLine($"{number}");
+        }
+
+        public static void PrintIntArray(int[] array)
+        {
+            string delimeter = " ";
+            int length = array.Length;
+            for (int i=0; i < length; i++)
+            {
+                if (i == length)
+                {
+                    delimeter = Environment.NewLine;
+                }
+                Console.Write($"{array[i]}{delimeter}");
+            }
+            Console.WriteLine();
+        }
+
+        public static void PrintDoubleArray(double[] array)
+        {
+            string delimeter = " ";
+            int length = array.Length;
+            for (int i = 0; i < length; i++)
+            {
+                if (i == length)
+                {
+                    delimeter = Environment.NewLine;
+                }
+                Console.Write($"{array[i]}{delimeter}");
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            int length;
+            int integer;
+            double rational;
+            int[] integers;
+            double[] doubles;
+            Console.WriteLine("Type integer:");
+            integer = ReadIntFromConsole();
+            Console.WriteLine("Your integer:");
+            PrintInt(integer);
+            Console.WriteLine("Type double:");
+            rational = ReadDoubleFromConsole();
+            Console.WriteLine("Your double:");
+            PrintDouble(rational);
+            Console.WriteLine("Type array's length:");
+            length = ReadIntFromConsole();
+            integers = new int[length];
+            doubles = new double[length];
+            for (int i=0; i < length; i++)
+            {
+                integers[i] = ReadIntFromConsole();
+            }
+            for (int i = 0; i < length; i++)
+            {
+                doubles[i] = ReadDoubleFromConsole();
+            }
+            Console.WriteLine("Your integers array:");
+            PrintIntArray(integers);
+            Console.WriteLine("Your doubles array:");
+            PrintDoubleArray(doubles);
+        }
+    }
+}
---
+++ Lesson3.1/Lesson3.3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> inputedValues = new Dictionary<int, int>();
+            int currentNumber;
+
+            do
+            {
+                Console.WriteLine("Type number:");
+                currentNumber = Convert.ToInt32(Console.ReadLine());
+                if (currentNumber != -1)
+                {
+                    if (inputedValues.ContainsKey(currentNumber))
+                    {
+                        inputedValues[currentNumber]++;
+                    }
+                    else
+                    {
+                        inputedValues.Add(currentNumber, 1);
+                    }
+                }
+            } while (currentNumber != -1);
+            Console.WriteLine("Found duplicates:");
+            foreach (KeyValuePair<int, int> kv in inputedValues)
+            {
+                if (kv.Value > 1)
+                {
+                    Console.WriteLine(string.Format("{0}", kv.Key));
+                }
+            }
+        }
+    }
+}
